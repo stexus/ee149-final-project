5afe5fde7aa843fb752c959e8548bbcafcbff717
Arthur Deng <arthurdeng@berkeley.edu>
2023-12-11 15:53:52 -0800

    distance syntax update, example added

--- human_following_robot/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.humanfollowingrobot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- human_following_robot/lib/backend/schema/structs/index.dart
+++
@@ -1,4 +0,0 @@
-export '/backend/schema/util/schema_util.dart';
-
-export 'b_t_devices_struct.dart';
-export 'magnetometer_values_struct.dart';
